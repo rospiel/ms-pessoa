97fd23feb2da0d7c658c1e0e90595fa49586f425
rospiel <rospielberg@gmail.com>
2021-10-05 13:26:49 -0300

    Registrando no eureka-serve

--- src/main/java/br/com/application/Application.java
+++ src/main/java/br/com/application/Application.java
@@ -2,10 +2,12 @@ package br.com.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 insert into tb_pessoa (uuid, nome, salario_dia) values ('f8df91f5-30b8-43e9-a440-087b748060ec', 'Rodrigo', 10.58);
+insert into tb_pessoa (uuid, nome, salario_dia) values ('cd615ac3-a10d-49d6-9dbe-b773740b4028', 'Sara', 105.58);
